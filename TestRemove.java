8746d5191194edc4b8d5617aded8c866925b67f1
Karlie27 <karliemmg@gmail.com>
2016-10-23 19:59:08 -0600

    Add files via upload

---
+++ TestRemove.java
@@ -0,0 +1,60 @@
+/** 
+    Joseline Ortiz 15039
+    Michelle Morales 15322
+	
+	TestRemove Prueba eliminar una cadena en la implementación de VectorHeap
+**/
+
+import static org.junit.Assert.*;
+
+import java.util.Vector;
+
+import org.junit.Test;
+
+public class TestRemove {
+
+	/**
+	 * Prueba eliminar una cadena en la implementacion VectorHeap.
+	 */
+	@Test
+	public void testRemove() {
+		VectorHeap<String> pQueue = new VectorHeap<String>();
+		pQueue.add("C");
+		pQueue.add("A");
+		
+		// Test: remover un elemento del VectorHeap
+		String result = pQueue.remove();
+		assertEquals("A", result); // Si se removio correctamente, debe regresar el valor con mayor prioridad, que es A.
+	}
+	
+	/**
+	 * Prueba eliminar un entero en la implementacion VectorHeap.
+	 */
+	@Test
+	public void testRemoveInt() {
+		VectorHeap<Integer> pQueue = new VectorHeap<Integer>();
+		pQueue.add(34);
+		pQueue.add(21);
+		
+		// Test: remover un elemento del VectorHeap
+		int result = pQueue.remove();
+		assertEquals(21, result); // Si se removio correctamente, debe regresar el valor con mayor prioridad, que es 21.
+	}
+	
+	/**
+	 * Prueba eliminar un objeto de tipo Paciente en la implementacion VectorHeap.
+	 */
+	@Test
+	public void testRemovePaciente() {
+		VectorHeap<Paciente> pQueue = new VectorHeap<Paciente>();
+		Paciente pPrueba = new Paciente("Sandra", "Cálculos renales", "B");
+		Paciente pPrueba2 = new Paciente("Melissa", "15 puñalas", "A");
+		pQueue.add(pPrueba);
+		pQueue.add(pPrueba2);
+		
+		// Test: remover un elemento del VectorHeap
+		Paciente result = pQueue.remove();
+		assertEquals(pPrueba2, result); // Si se removio correctamente, debe regresar el valor con mayor prioridad, que es pPrueba2.
+	}
+
+}
